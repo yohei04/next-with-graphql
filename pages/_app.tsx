714d726b0b5c5958ada4f5360045d7d65ae9dfc8
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-11-28 22:18:33 +0900

    fix: move apollo client file

---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  InMemoryCache,
+} from '@apollo/client';
+import '../styles/globals.css';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,19 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { gql, useQuery } from '@apollo/client';
-import ExchangeRates from 'components/ExchangeRates';
+
 import AddTodo from 'components/graphql/AddTodo';
 import App from 'components/context/App';
-
-const client = new ApolloClient({
-  uri: 'https://sxewr.sse.codesandbox.io/',
-  cache: new InMemoryCache(),
-});
-
-// client.query({
-//   query: gql`
-//     query GetRates {
-//       rates(currency: "USD") {
-//         currency
-//       }
-//     }
-//   `,
-// });
+import ExchangeRates from 'components/graphql/ExchangeRates';
 
 export default function Home() {
   return (
-    <ApolloProvider client={client}>
-      <div>
-        {/* <h2>My first Apollo app 🚀</h2>
-        <ExchangeRates />
-        <AddTodo /> */}
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+      {/* <AddTodo /> */}
 
-        {/* Context */}
-        <App />
-      </div>
-    </ApolloProvider>
+      {/* Context */}
+      {/* <App /> */}
+    </div>
   );
 }
